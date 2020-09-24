a0e8883caf6dd822a52d1d3d984a5ce3f2b1b77b
symonne taylor <safiya@symonnes-mbp.home>
2020-09-24 14:45:07 +0100

    book store

---
+++ books.js
@@ -0,0 +1,211 @@
+
+ 
+ const books = [{
+     name: 'Queenie',
+     author: 'Candice Carty-Williams',
+     price: '£0.99',
+     description: `'THE SUNDAY TIMES BESTSELLER SHORTLISTED FOR THE COSTA FIRST NOVEL AWARD LONGLISTED FOR THE WOMEN'S PRIZE FOR FICTION
+      'Hilarious, compelling, painful, enlightening, honest. I loved it' - Dolly Alderton 
+      'Brilliant, timely, funny, heartbreaking' - Jojo Moyes 'A deliciously funny, characterful, topical ...',`,
+     image: 'http://books.google.com/books/content?id=NhBmDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+ },
+ {
+     name: 'Ghana Must Go',
+     author: 'Taiye Selasi',
+     price: '£2.99',
+     description: `'A stunning novel, spanning generations and continents, 
+     Ghana Must Go by rising star Taiye Selasi is a tale of family drama and forgiveness, 
+     for fans of Zadie Smith and Chimamanda Ngozi Adichie. This is the story of a family -- of the simple, 
+     devastating ways in which families tear themselves apart, ...'`,
+     image: 'http://books.google.com/books/content?id=9j4JGEkCA5cC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api';
+ },
+ {
+     name: 'The Girl Who Smiled Beads',
+     author: 'Clemantine Wamariya, Elizabeth Weil',
+     price: '£3.99',
+     description: `'When Clemantine Wamariya was six years old, her world was torn apart. 
+     She didn't know why her parents began talking in whispers, 
+     or why her neighbours started disappearing, or why she could hear distant thunder'`,
+     image: 'http://books.google.com/books/content?id=djc_DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+ },
+ {
+    name: 'Americanah',
+    author: 'Chimamanda Ngozi Adichie',
+    price: '£3.49',
+    description: `'SHORTLISTED FOR THE BAILEY’S WOMEN’S PRIZE FOR FICTION
+     ‘A delicious, important novel’ The Times ‘Alert, alive and gripping’ 
+     Independent ‘Some novels tell a great story and others make you change the way you look at the world. Americanah does both.’ Guardian...'`,
+    image: 'http://books.google.com/books/content?id=QXHwNEB2CkYC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+ },
+ {
+    name: 'What We Lose',
+    author: 'Zinzi Clemmons',
+    price: '£2.99',
+    description: `'A short, intense and profoundly moving novel about race, identity, family, sex, country, and death – from one of the National Book Foundation’s 5 Under 35: an author of rare, haunting power this is a stunning debut about a young African-American woman coming of age. 
+    Raised in Pennsylvania, Thandi views the world of her mother’s childhood in Johannesburg as both impossibly distant and ever present. She is an outsider wherever she goes, caught between being black and white...
+    `,
+    image: 'http://books.google.com/books/content?id=NWrpDQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+
+ },
+ {
+    name: 'The Good Lord Bird',
+    author: 'by James McBride',
+    price: '£8.38',
+    description:`'Soon to be a Showtime limited series starring Ethan Hawke Winner of the National Book Award for Fiction A Washington Post, Publishers Weekly, Oprah Magazine Top 10 Book of the Year From the bestselling author of The Color of Water, Song Yet Sung, Five-Carat Soul, and Kill 'Em and Leave, a James Brow...'`,
+    image: 'http://books.google.com/books/content?id=BnudfIm9NMUC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+ },
+ {
+    name: 'Ordinary Light',
+    author: 'John S. Green',
+    price: '£3.83',
+    description: `'Real, raw, and funny, the voice of John S. Green takes you on a lifelong journey of seeking sacred resting places in the middle of lifes ever increasing chaos and distractions. 
+    Headlines scream, Beware! Look out! The end is near! A lightning, flashing, neon pulse that blinds us from the ordinary lig...'`,
+    image: 'http://books.google.com/books/content?id=xeHGDgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+
+ },
+ {
+    name: 'The Water Dancer',
+    author: 'Ta-Nehisi Coates',
+    price: '£6.99',
+    description: `'The unmissable debut novel by the critically acclaimed author of Between the World and Me andWe Were Eight Years in Power - a richly imagined and compulsively page-turning journey to freedom OPRAH BOOK CLUB PICK 'I haven't felt this way since I first read Beloved... I wish Toni [Morrison] was alive ...'`,
+    image: 'http://books.google.com/books/content?id=f2KMDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+        
+ },
+ { 
+    name: 'Known and Strange Things',
+    author: 'Teju Cole',
+    price: '£5.03',
+    description: `'A blazingly intelligent first collection of essays from the award-winning author of Open City and Every Day Is for the Thief. With these pieces on politics, photography, travel, history and literature - many of which have become viral sensations, shared and debated around the globe - Teju Cole solid...'`,
+    image: 'http://books.google.com/books/content?id=tiSPDAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  },
+  {
+    name: 'Black Is the Body',
+    author: 'by Emily Bernard',
+    price: '£3.63',
+    description: `'An extraordinary, exquisitely written memoir (of sorts) that looks at race--in a fearless, penetrating, honest, true way--in twelve telltale, connected, deeply personal essays that explore, up-close, the complexities and paradoxes, the haunting memories and ambushing realities of growing up black in...'`,
+    image: 'http://books.google.com/books/content?id=OqJaDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  },
+  {
+    name: `'It's Trevor Noah: Born a Crime'`,
+    author: 'Trevor Noah',
+    price: '£3.99',
+    description: `'The host of The Daily Show, Trevor Noah, tells the story of growing up mixed race in South Africa under and after apartheid in this young readers' adaptation of his bestselling adult memoir Born a Crime: Stories from a South African Childhood. BORN A CRIME IS SOON TO BE A MAJOR MOTION PICTURE STARRI...'`,
+    image: 'http://books.google.com/books/content?id=Kv16DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  },
+  {
+    name: 'Zone One',
+    author: 'Colson Whitehead',
+    price: '£4.99',
+    description: `'From the author of the Man Booker longlisted The Underground Railroad A pandemic has devastated the planet, sorting humanity into two types: the uninfected and the infected, the living and the living dead. The worst of the plague is now past, and Manhattan is slowly being resettled. Armed forces hav...'`,
+    image:  'http://books.google.com/books/content?id=udzdZ7oHIdgC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  },
+  {
+    name: 'We Cast a Shadow',
+    author: 'Maurice Carlos Ruffin',
+    price: '£4.05',
+    description: `'“An incisive and necessary” (Roxane Gay) debut for fans of Get Out and Paul Beatty’s The Sellout, about a father’s obsessive quest to protect his son—even if it means turning him white Longlisted for the Center for Fiction First Novel Prize • “Stunning and audacious . . . at once a pitch-black comed...'`,
+    image: 'http://books.google.com/books/content?id=CZJbDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  },
+  {
+    name: 'Black Leopard, Red Wolf',
+    author: 'Marlon James',
+    price: '£4.99',
+    description: `'The Sunday Times Number 1 Bestseller and A New York Times Bestseller 'Black Leopard, Red Wolf is the kind of novel I never realized I was missing until I read it. A dangerous, hallucinatory, ancient Africa, which becomes a fantasy world as well-realized as anything Tolkien made, with language as pow...'`,
+    image: 'http://books.google.com/books/content?id=ExJdDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  },
+  {
+    name: 'More Than Enough',
+    author: 'Elaine Welteroth',
+    price: '£4.99',
+    description: `'In this part-manifesto, part-memoir, the revolutionary editor who infused social consciousness into the pages of Teen Vogue explores what it means to come into your own – on your own terms. Elaine Welteroth has climbed the ranks of media and fashion, shattering ceilings along the way. In this riveti...'`,
+    image: 'http://books.google.com/books/content?id=vE6MDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  },
+  {
+    name: 'How Beautiful We Were',
+    author: 'Imbolo Mbue',
+    price: 'No Retail Price',
+    description: `''A formidable storyteller' JONATHAN FRANZEN Set in the fictional African village of Kosawa, How Beautiful We Were tells the story of a people living in fear amidst environmental degradation wrought by an American oil company. Pipeline spills have rendered farmlands infertile. Children are dying from...'`,
+    image: 'http://books.google.com/books/content?id=GNjBDwAAQBAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+  },
+  {
+    name: 'Behold the Dreamers',
+    author: 'Imbolo Mbue',
+    price: '£3.99',
+    description: `'A NEW YORK TIMES BOOK OF THE YEAR AN OBSERVER BOOK OF THE YEAR OPRAH BOOK CLUB PICK A powerful and timely story of marriage, class, race and the pursuit of the American Dream. Behold the Dreamers is a dazzling debut novel about life, liberty and the pursuit of happiness – and of what we’re prepared ...'`,
+    image: 'http://books.google.com/books/content?id=R16QCgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  },
+  {
+    name: 'What Is Not Yours Is Not Yours',
+    author: 'Helen Oyeyemi',
+    price: '£6.99',
+    description: `'The stories collected in What Is Not Yours Is Not Yours are linked by more than the exquisitely winding prose of their creator: Helen Oyeyemi's ensemble cast of characters slip from the pages of their own stories only to surface in another. The reader is invited into a world of lost libraries and lo...'`,
+    image: 'http://books.google.com/books/content?id=ELG6CgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  },
+  {
+    name: 'Homegoing',
+    author: 'Yaa Gyasi',
+    price: '£3.99',
+    description: `'THE SUNDAY TIMES BESTSELLER Selected for Granta's Best of Young American Novelists 2017 Winner of the National Book Critics Circle Award for Best First Book Shortlisted for the PEN/Robert W. Bingham Prize for Debut Fiction Effia and Esi: two sisters with two very different destinies. One sold into s...'`,
+    image: 'http://books.google.com/books/content?id=L7c4CwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  },
+  {
+    name: 'Red at the Bone',
+    author: 'Jacqueline Woodson',
+    price: '£7.99',
+    description: `'THE TOP 10 NEW YORK TIMES BESTSELLER LONGLISTED FOR THE WOMEN'S PRIZE 2020 'Will remind you why you love reading' Stylist (Best New Books for 2020) 'Sublime... will strike you in the heart' Candice Carty-Williams, author of Queenie 'Profound, moving and consistently unexpected... Pure poetry' Observ...'`,
+    image: 'http://books.google.com/books/content?id=GLisDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  },
+  {
+    name: 'I Almost Forgot About You',
+    author: 'Terry McMillan',
+    price: '£7.05',
+    description: `'NEW YORK TIMES BESTSELLER • The bestselling author of How Stella Got Her Groove Back and Waiting To Exhale is back with the inspiring story of a woman who shakes things up in her life to find greater meaning NAMED ONE OF THE BEST BOOKS OF THE YEAR BY LIBRARY JOURNAL In I Almost Forgot About You, Dr....'`,
+    image: 'http://books.google.com/books/content?id=O9yKCgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+  },
+  {
+    name: 'The Girl with the Louding Voice',
+    author: 'Abi Daré',
+    price: '£4.99',
+    description: `'All you have are your words. AS RECOMMENDED BY MALALA YOUSAFZAI AN INSTANT NEW YORK TIMES BESTSELLER A STYLIST BEST BOOK OF 2020 'A stunning novel - original, beautiful and powerful' Rosamund Lupton Meet Adunni, a teenage girl born into a rural Nigerian village. Aged fourteen, she is a commodity, a ...'`,
+    image: 'http://books.google.com/books/content?id=jXqaDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+  },
+  {
+    name: 'Well-Read Black Girl',
+    author: 'Glory Edim',
+    price: '£3.99',
+    description: `''Required reading.' - Cosmopolitan 'This should be read as a sacred text. Here, you will bear witness to a perpetual salvation song.' - Jason Reynolds Remember that moment when you first encountered a character who seemed to be written just for you? That feeling of belonging remains with readers the...'`,
+    image: 'http://books.google.com/books/content?id=eD90DwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+  },
+  {
+    name: 'Such a Fun Age',
+    author: 'Kiley Reid',
+    price: '£9.09',
+    description: `'A new literary star' The Times A Reese Witherspoon Book Club Pick // A Zoella Book Club Pick // A Times, Stylist, Elle, Glamour and BBC Best Book of 2020 When Emira is apprehended at a supermarket for 'kidnapping' the white child she's actually babysitting, it sets off an explosive chain of events. ...'`,
+    image: 'http://books.google.com/books/content?id=fjadDwAAQBAJ&printsec=frontcover&img=1&zoom=5&edge=curl&source=gbs_api',
+  },
+  {
+    name: 'Goldilocks and the Three Bears',
+    author: 'Calee M. Lee',
+    price: '£9.64',
+    description: `'Discover Fairy Tales Someone has been eating my porridge! The classic fairy tale of Goldilocks and the Three Bears has been retold with simple, rhythmic sentences for beginning readers. Discover Fairy Tales are familiar stories with cute illustrations, perfect for the touchscreen generation....'`,
+    image: 'http://books.google.com/books/content?id=CrGwCt3e0LMC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+ }];
+
+ function displayBooks (bookList) {
+    const bookstore = document.querySelector('#bookstore');
+
+    bookList.forEach(book => {
+      const listItem = document.createElement('li');
+      const text = document.createTextNode(book.name);
+      const author = document.createTextNode(book.author);
+      const image = document.createElement('img');
+      image.src = book.image;
+
+      listItem.appendChild(text);
+      listItem.appendChild(author);
+      listItem.appendChild(image);
+    });
+
+}
+
+displayBooks(books);
